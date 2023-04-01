7cf83d5b9daca5ab2fcc68a62e4d691043a1e6e6
fire833 <kttpsy@gmail.com>
2023-04-01 15:27:11 -0500

    [graph] Worked on instruction implementations
    Signed-off by: Kendall Tauser <kttpsy@gmail.com>

--- src/components/lang/graph.ts
+++ src/components/lang/graph.ts
@@ -1,18 +1,42 @@
 
 type GraphNodeID = string;
 export class GraphNode {
-	id: GraphNodeID
+	id: GraphNodeID;
 	value: number = 0;
 	neighbors: GraphNodeID[] = [];
 
-	public constructor(id: GraphNodeID) {
+	constructor(id: GraphNodeID, value: number);
+	constructor(id: GraphNodeID);
+	constructor(id: GraphNodeID, value?: number) {
 		this.id = id;
+
+		if (value) {
+			this.value = value;
+		}
+	}
+
+	public add_neighbor(id: GraphNodeID) {
+		this.neighbors.push(id);
+	}
+
+	public remove_neighbor(id: GraphNodeID) {
+		let index: number = -1;
+
+		this.neighbors.forEach((v, i, a) => {
+			if (v === id) {
+				index = i;
+			}
+		});
+
+		if (index != -1) {
+			this.neighbors.splice(index, 1);
+		}
 	}
 }
 
 // GraphContext is the primary data structure to store graph execution state.
 export class GraphContext {
-	graph: {[id: GraphNodeID]: GraphNode} = {};
+	graph: { [id: GraphNodeID]: GraphNode } = {};
 	active_node_id: GraphNodeID;
 	root_node_id: GraphNodeID;
 
@@ -41,15 +65,14 @@ export class GraphContext {
 		`
 	}
 
-
 	bfs(pred: (node: GraphNode, visited: Set<GraphNodeID>, queued: Set<GraphNodeID>) => void) {
 		let queue = [this.root_node_id];
 		let queued_set = new Set<GraphNodeID>([this.root_node_id]);
 		let visited_set = new Set<GraphNodeID>();
 
-		while(queue.length) {
+		while (queue.length) {
 			let popped_id = queue.shift();
-			if(!popped_id) {
+			if (!popped_id) {
 				throw "Tried to shift on empty queue in BFS";
 			};
 			let popped_node = this.graph[popped_id];
@@ -70,9 +93,103 @@ export class GraphContext {
 		return this.graph[this.active_node_id];
 	}
 
-	constructor(nodes: {[id: GraphNodeID]: GraphNode}, root_id: GraphNodeID) {
+	constructor(nodes: { [id: GraphNodeID]: GraphNode }, root_id: GraphNodeID) {
 		this.graph = nodes;
 		this.active_node_id = root_id;
 		this.root_node_id = root_id;
 	}
+
+	// public-facing instruction implementations
+
+	// Returns the current u32 stored within the active node.
+	public value(): number {
+		return this.graph[this.active_node_id].value;
+	}
+
+	// Sets active node to the current value of P1.
+	public set(P1: number) {
+		this.graph[this.active_node_id].value = P1;
+	}
+
+	// // Adds P1 and P2 together and returns the result.
+	// public math_add(P1: number, P2: number): number {
+	// 	return P1 + P2;
+	// }
+
+	// // Multiplies P1 and P2 together and returns the result.
+	// public math_mult(P1: number, P2: number): number {
+	// 	return P1 * P2;
+	// }
+
+	// // Divides P1 by P2 and returns the result. Will return the floor of the division.
+	// public math_div(P1: number, P2: number): number {
+	// 	return P1 / P2;
+	// }
+
+	// Append a neighbor to the active node with a preset value of P1.
+	public bubble(P1: number) {
+		let node = new GraphNode(crypto.randomUUID(), P1);
+		this.graph[this.active_node_id].neighbors.push(node.id);
+		this.graph[node.id] = node;
+	}
+
+	// Set the active node to the neighbor at index P1 of the currently active node. 
+	// Returns an error if P1 is not a valid index.
+	public traverse(P1: number) {
+		this.active_node_id = this.graph[this.active_node_id].neighbors[P1];
+	}
+
+	// Returns the number of neighbors attached to the currently active node.
+	public num_neighbors(): number {
+		return this.graph[this.active_node_id].neighbors.length;
+	}
+
+	// Sets neighbor P2 to have the index of P1 and neighbor P1 to have the index of P2.
+	public reorder(P1: number, P2: number) {
+		let active = this.graph[this.active_node_id];
+
+		let p1val = active.neighbors[P1];
+		let p2val = active.neighbors[P2];
+		active.neighbors[P1] = p2val;
+		active.neighbors[P2] = p1val;
+	}
+
+	// Sets the value of the the root node as the value of the current active node. 
+	// This does not set the root as the active node.
+	public set_root() {
+		this.root_node_id = this.active_node_id;
+	}
+
+	// Sets the value of neighbor with index P1 to the value P2.
+	public set_neighbor(P1: number, P2: number) {
+		let neighbor = this.graph[this.active_node_id].neighbors[P1];
+		this.graph[neighbor].value = P2;
+	}
+
+	// Returns the current value of neighbor with index P1.
+	public get_neighbor(P1: number): number {
+		let neighbor = this.graph[this.active_node_id].neighbors[P1];
+		return this.graph[neighbor].value;
+	}
+
+	// Remove the neighbor at index P1 from the graph. All edges of said node 
+	// will be removed from the graph and the value of the node will be lost.
+	public delete_neighbor(P1: number) {
+		let dying_neighbor = this.graph[this.active_node_id].neighbors[P1];
+
+		this.graph[dying_neighbor].neighbors.forEach((v, i, a) => {
+			this.graph[v].remove_neighbor(dying_neighbor);
+		})
+
+		delete this.graph[dying_neighbor];
+	}
+
+	// Remove the neighbor at index P1 from the graph. All neighbors of the cut 
+	// node will be connected. For every pair of nodes which were neighbors of 
+	// the cut node, a new edge will be created between them if it does not already 
+	// exist.
+	public cut_neighbor(P1: number) {
+		let dying_neighbor = this.graph[this.active_node_id].neighbors[P1];
+	}
+
 }
--- src/components/results/debug_bar.tsx
+++ src/components/results/debug_bar.tsx
@@ -1,4 +1,5 @@
 
+import { useState } from "react";
 import styles from "./debug_bar.module.css";
 
 export interface DebugBarProps {
@@ -11,11 +12,32 @@ export interface DebugBarProps {
 }
 
 export default function DebugBar(props: DebugBarProps) {
+
+    // state for managing where the debug bar lives in the world.
+    const [xCoord, setX] = useState<number>(0);
+    const [yCoord, setY] = useState<number>(0);
+
+    const [xOffSet, setXOffset] = useState<number>(0);
+    const [yOffSet, setYOffset] = useState<number>(0);
+
+    const handleDrag = (e: React.DragEvent<HTMLDivElement>) => {
+        let rect = e.currentTarget.getBoundingClientRect();
+        if (e.type === "dragstart") {
+            setXOffset(e.clientX - rect.x);
+            setYOffset(e.clientY - rect.y);
+            return;
+        }
+
+        setX(e.clientX - xOffSet);
+        setY(e.clientY - yOffSet);
+    }
+
     return (
         <div className={styles.debug} draggable={true}
-            onDragStart={() => { }}
-            onDrag={() => { }}
-            onDragEnd={() => { }}
+            style={{ position: "absolute", left: xCoord, top: yCoord }}
+            onDragStart={(e) => { handleDrag(e); e.dataTransfer.setDragImage(document.createElement("div"), xCoord, yCoord); }}
+            onDrag={handleDrag}
+            onDragEnd={handleDrag}
         >
             <button onClick={props.onCompile}>Compile stuff</button>
             <button onClick={props.onRun}>Run</button>
