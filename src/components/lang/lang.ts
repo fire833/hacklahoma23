20d973ec89fd88bed74c7923118080e8bd4af566
aaronp <aaronp@ou.edu>
2023-04-01 12:38:20 -0500

    prepare to add compilation

--- src/components/lang/lang.ts
+++ src/components/lang/lang.ts
@@ -2,23 +2,41 @@ import { languages } from "monaco-editor";
 
 export const LANG = "GraphASM";
 
-export interface FuncDef {
+export interface ProgramState {
 
+}
+
+export interface FuncDef {
+    evaluate: (state: ProgramState) => void
 }
 
 export const FunctionDefinitions: { [funcname: string]: FuncDef } = {
     "SET": {
-
+        evaluate: (state: ProgramState) => {
+            
+        }
     }
 }
 
 export const LANG_DEF: languages.IMonarchLanguage = {
+    keywords: Object.keys(FunctionDefinitions),
     tokenizer: {
         root: [
             [/^[a-zA-Z]+:/, "tag"],
-            [/[A-Z$]\w+/, "keyword"],
+            [/-?[0-9]+/, "constant"],
+            [/[A-Z$]\w+/, {
+                cases: {
+                    '@keywords': 'keyword',
+                    'default': 'invalid'
+                }
+            }],
             [/\s/, "whitespace"],
             [/#.*/, "comment"]
         ],
     }
+}
+
+
+export const compile = (source: string) => {
+    
 }
