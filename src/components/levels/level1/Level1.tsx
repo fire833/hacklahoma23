dd99f3c8f7ad321330e445ecc809876f4f69a51e
aaronp <aaronp@ou.edu>
2023-04-01 23:00:58 -0500

    Merge tabs

--- src/components/levels/level1/Level1.tsx
+++ src/components/levels/level1/Level1.tsx
@@ -14,36 +14,6 @@ const LEVEL_NUM = 1;
 const LUNCH_TIME = 1200
 
 export function Level1() {
-    const tabs: Tab[] = [{
-        tab_kind: "react_node",
-        tab_name: "Level 1",
-        node: <div style={{ padding: "2%" }}>
-            <h1>Level 1 - Lunchtime</h1>
-            <p>
-                You wake up for another morning of imprisonment in the intergalactic prison A-77. You have noticed for the last
-                few days that the countdown timer above your cell door always drops down to zero before your door unlocks and you
-                are able to go to recess/meals.
-            </p>
-            <p>
-                You wonder if there is a way to modify the timer in order to get out of your cell
-                early. As you are rooting around in your computer, you find a program called <i>tedit</i> (your initial program state
-                to the right). You wonder if you can set the primary register (the root/active node) to 0, then maybe your door will
-                open.
-            </p>
-            <h4>Goals/Objectives</h4>
-            <p>
-                The goal for this challenge is to assign values to nodes with the <i>SET</i> instruction. You can do this by writing
-            </p>
-            <b>SET 0</b>
-            <p>
-                Within your program terminal, and try and run it. If your graph pane lights up green, then you the register was successfully
-                set to zero and your door opened!
-            </p>
-        </div>
-    }, {
-        tab_kind: "editor",
-        tab_name: "Editor"
-    }]
 
     const test_cases: TestCase[] = [
         {
@@ -85,7 +55,7 @@ export function Level1() {
     ];
     const [loadedTestCase, setLoadedTestCase] = useState(0);
 
-    
+
     const [correctTestCases, setCorrectTestCases] = useState<number[]>([]);
 
     function completeCase(number: number) {
@@ -96,9 +66,28 @@ export function Level1() {
     const tabs: Tab[] = [{
         tab_kind: "react_node",
         tab_name: "Level 1",
-        node: <div>
+        node: <div style={{ padding: "2%" }}>
             <h1>Level 1 - Lunchtime</h1>
-            <p>You wake up for another morning of imprisonment in the intergalactic prison A-77.</p>
+            <p>
+                You wake up for another morning of imprisonment in the intergalactic prison A-77. You have noticed for the last
+                few days that the countdown timer above your cell door always drops down to zero before your door unlocks and you
+                are able to go to recess/meals.
+            </p>
+            <p>
+                You wonder if there is a way to modify the timer in order to get out of your cell
+                early. As you are rooting around in your computer, you find a program called <i>tedit</i> (your initial program state
+                to the right). You wonder if you can set the primary register (the root/active node) to 0, then maybe your door will
+                open.
+            </p>
+            <h4>Goals/Objectives</h4>
+            <p>
+                The goal for this challenge is to assign values to nodes with the <i>SET</i> instruction. You can do this by writing
+            </p>
+            <b>SET 0</b>
+            <p>
+                Within your program terminal, and try and run it. If your graph pane lights up green, then you the register was successfully
+                set to zero and your door opened!
+            </p>
         </div>
     }, {
         tab_kind: "editor",
@@ -107,14 +96,13 @@ export function Level1() {
     {
         tab_kind: "react_node",
         tab_name: "Test Cases",
-        node: <div style={{padding: "1em"}}>
-        {test_cases.map((e, ind) => <div>
-            <TestCaseLoader activate={() => setLoadedTestCase(ind)} index={ind} testCase={e} isActive={loadedTestCase === ind} isComplete={correctTestCases.indexOf(ind) !== -1}></TestCaseLoader>
-        </div>)}
+        node: <div style={{ padding: "1em" }}>
+            {test_cases.map((e, ind) => <div>
+                <TestCaseLoader activate={() => setLoadedTestCase(ind)} index={ind} testCase={e} isActive={loadedTestCase === ind} isComplete={correctTestCases.indexOf(ind) !== -1}></TestCaseLoader>
+            </div>)}
         </div>
     }
-    ]
-
+    ];
 
     return <Level completeCase={completeCase} completedTestCased={correctTestCases} tabs={tabs} loadedTestCase={loadedTestCase} setLoadedTestCase={setLoadedTestCase} test_cases={test_cases}></Level>
 }
