00b3eedba789cfb2ef76ccd1e8a7a06225abd1b0
fire833 <kttpsy@gmail.com>
2023-04-01 22:28:16 -0500

    [lore] Worked on level one description/lore
    Signed-off by: Kendall Tauser <kttpsy@gmail.com>

--- src/components/levels/level1/Level1.tsx
+++ src/components/levels/level1/Level1.tsx
@@ -16,15 +16,33 @@ export function Level1() {
     const tabs: Tab[] = [{
         tab_kind: "react_node",
         tab_name: "Level 1",
-        node: <div>
+        node: <div style={{ padding: "2%" }}>
             <h1>Level 1 - Lunchtime</h1>
-            <p>You wake up for another morning of imprisonment in the intergalactic prison A-77.</p>
+            <p>
+                You wake up for another morning of imprisonment in the intergalactic prison A-77. You have noticed for the last
+                few days that the countdown timer above your cell door always drops down to zero before your door unlocks and you
+                are able to go to recess/meals.
+            </p>
+            <p>
+                You wonder if there is a way to modify the timer in order to get out of your cell
+                early. As you are rooting around in your computer, you find a program called <i>tedit</i> (your initial program state
+                to the right). You wonder if you can set the primary register (the root/active node) to 0, then maybe your door will
+                open.
+            </p>
+            <h4>Goals/Objectives</h4>
+            <p>
+                The goal for this challenge is to assign values to nodes with the <i>SET</i> instruction. You can do this by writing
+            </p>
+            <b>SET 0</b>
+            <p>
+                Within your program terminal, and try and run it. If your graph pane lights up green, then you the register was successfully
+                set to zero and your door opened!
+            </p>
         </div>
     }, {
         tab_kind: "editor",
         tab_name: "Editor"
-    }
-    ]
+    }]
 
     return <Level tabs={tabs} test_cases={[
         {
